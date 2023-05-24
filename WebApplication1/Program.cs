53192ca6925914c2535ef36d99b9ad00522cc451
Austin Nguyen <austinkn123@gmail.com>
2023-05-24 14:14:30 -0500

    Modified Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -4,7 +4,6 @@ using Newtonsoft.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 //JSON Serializer to detault data view to JSON
 builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
