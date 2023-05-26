3b1739d001bb61381489145dd7f9af3656eddbf9
Austin Nguyen <austinkn123@gmail.com>
2023-05-26 14:03:26 -0500

    Finished department controller

--- WebApplication1/Controllers/DepartmentController.cs
+++ WebApplication1/Controllers/DepartmentController.cs
@@ -12,11 +12,13 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class DepartmentController : ControllerBase
     {
+        private readonly ILogger<DepartmentController> _logger;
         public readonly IConfiguration _config;
 
         //This is the contructor that gets the sql server connection string in the appsettings.json
-        public DepartmentController(IConfiguration config)
+        public DepartmentController(ILogger<DepartmentController> logger, IConfiguration config)
         {
+            _logger = logger;
             _config = config;
         }
 
@@ -24,27 +26,115 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Department>>> GetAllDepartments()
         {
-            //Opens a new sql connection
-            using var connection = new SqlConnection(_config.GetConnectionString("EmployeeAppCon"));
+            _logger.LogInformation("Received GET request");
 
+            try
+            {
+                //Opens a new sql connection
+                using var connection = new SqlConnection(_config.GetConnectionString("EmployeeAppCon"));
 
-            var departments = await connection.QueryAsync<Department>("select * from Department");
+                var departments = await connection.QueryAsync<Department>("select * from Department");
 
-            return Ok(departments);
+                return Ok(departments);
+            }
+            catch (Exception err)
+            {
+                _logger.LogError(err.Message);
+                return StatusCode(500, err.Message);
+            }
         }
 
         //Get a specific department based off id
         [HttpGet("{department_Id}")]
         public async Task<ActionResult<Department>> GetDepartment(int department_Id)
         {
-            //Opens a new sql connection
-            using var connection = new SqlConnection(_config.GetConnectionString("EmployeeAppCon"));
+            _logger.LogInformation("Received GET request");
 
+            try
+            {
+                //Opens a new sql connection
+                using var connection = new SqlConnection(_config.GetConnectionString("EmployeeAppCon"));
 
-            var departments = await connection.QueryAsync<Department>("select * from Department where DepartmentId = @DepartmentId", 
-                new { DepartmentId = department_Id });
+                var departments = await connection.QueryAsync<Department>("select * from Department where DepartmentId = @DepartmentId", 
+                    new { DepartmentId = department_Id });
 
-            return Ok(departments);
+                return Ok(departments);
+            }
+            catch (Exception err)
+            {
+                _logger.LogError(err.Message);
+                return StatusCode(500, err.Message);
+            }
+
+            
+        }
+
+        //Create a department
+        [HttpPost]
+        public async Task<ActionResult> CreateDepartment(Department department)
+        {
+            _logger.LogInformation("Received POST request");
+
+            try
+            {
+                //Opens a new sql connection
+                using var connection = new SqlConnection(_config.GetConnectionString("EmployeeAppCon"));
+
+                await connection.ExecuteAsync("insert into Department (DepartmentName) values (@DepartmentName)", department);
+            }
+            catch (Exception err)
+            {
+                _logger.LogError(err.Message);
+                return StatusCode(500, err.Message);
+            }
+            
+
+            return Ok("Created Successfully!");
+        }
+
+        //Update a department
+        [HttpPut]
+        public async Task<ActionResult> UpdateDepartment(Department department)
+        {
+            _logger.LogInformation("Received PUT request");
+
+            try
+            {
+                //Opens a new sql connection
+                using var connection = new SqlConnection(_config.GetConnectionString("EmployeeAppCon"));
+
+                await connection.ExecuteAsync("update Department set DepartmentName = @DepartmentName where DepartmentId = @DepartmentId", department);
+            }
+            catch (Exception err)
+            {
+                _logger.LogError(err.Message);
+                return StatusCode(500, err.Message);
+            }
+
+            return Ok("Updated Successfully!");
+        }
+
+        //Delete a department
+        [HttpDelete("{department_Id}")]
+        public async Task<ActionResult> DeleteDepartment(int department_Id)
+        {
+            _logger.LogInformation("Received PUT request");
+
+            try
+            {
+                //Opens a new sql connection
+                using var connection = new SqlConnection(_config.GetConnectionString("EmployeeAppCon"));
+
+                await connection.ExecuteAsync("delete from Department where DepartmentId = @DepartmentId", new {DepartmentId = department_Id });
+            }
+            catch(Exception err)
+            {
+                _logger.LogError(err.Message);
+                return StatusCode(500, err.Message);
+            }
+            
+
+            return Ok("Deleted Successfully!");
         }
 
 
